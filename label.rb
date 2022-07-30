e45ab7a6a0a8a5bfdb68df85d46bb1645ed3523e
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-07-30 12:11:05 +0100

    Fix linter errors

--- label.rb
+++ label.rb
@@ -1,13 +1,13 @@
 class Label
-    def initialize(title, color)
-      @id = id
-      @title = title
-      @color = color
-      @items = []
-    end
-  
-    def add_item(item)
-      @items << item unless @items.include?(item)
-      item.label = self
-    end
+  def initialize(title, color)
+    @id = id
+    @title = title
+    @color = color
+    @items = []
   end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.label = self
+  end
+end
